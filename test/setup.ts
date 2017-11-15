be18f39d629886ac1a4833ca8be074104c7ea699
Saeed Ganji <shahabganji@gmail.com>
2017-11-15 21:59:47 +0330

    error( jsondom is not a function )

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,7 @@
 import 'aurelia-polyfills';
 import { Options } from 'aurelia-loader-nodejs';
-// import { initialize } from 'aurelia-pal-nodejs';
-import { initialize } from 'aurelia-pal-browser';
+import { initialize } from 'aurelia-pal-nodejs';
+// import { initialize } from 'aurelia-pal-browser';
 import * as path from 'path';
 Options.relativeToDir = path.join(__dirname, 'unit');
 initialize();
