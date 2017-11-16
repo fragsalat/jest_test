ead472f8533524f2f47c21e6c37552671f86523f
Thomas Schlage <thomas.schlage@zalando.de>
2017-11-16 17:07:38 +0100

    Fixed tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,6 @@
 import 'aurelia-polyfills';
 import { Options } from 'aurelia-loader-nodejs';
-import { initialize } from 'aurelia-pal-nodejs';
-// import { initialize } from 'aurelia-pal-browser';
+import { globalize } from 'aurelia-pal-nodejs';
 import * as path from 'path';
-Options.relativeToDir = path.join(__dirname, 'unit');
-initialize();
+Options.relativeToDir = path.join(__dirname, '');
+globalize();
